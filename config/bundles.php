6eaff655249ec55fe594a59814609eb05099c053
Javier <fjmoyano07@gmail.com>
2020-10-20 15:30:46 +0200

    nelmio cors add

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
